8374a5ce9e761145cf5628debd3edaa4f677aafa
Jonathan <jonathan.pastor@me.com>
2016-10-06 14:04:39 +0200

    add two functions to the cluster's API;

--- common_dibbs/clients/rm_client/apis/cluster_definitions_api.py
+++ common_dibbs/clients/rm_client/apis/cluster_definitions_api.py
@@ -148,6 +148,112 @@ class ClusterDefinitionsApi(object):
                                             callback=params.get('callback'),
                                             _return_http_data_only=params.get('_return_http_data_only'))
 
+    def clusters_id_add_host_post(self, id, **kwargs):
+        """
+        Add a host to the specified cluster.
+        Add a host to the specified cluster. 
+
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please define a `callback` function
+        to be invoked when receiving the response.
+        >>> def callback_function(response):
+        >>>     pprint(response)
+        >>>
+        >>> thread = api.clusters_id_add_host_post(id, callback=callback_function)
+
+        :param callback function: The callback function
+            for asynchronous request. (optional)
+        :param int id: Id of the cluster (required)
+        :return: Cluster
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('callback'):
+            return self.clusters_id_add_host_post_with_http_info(id, **kwargs)
+        else:
+            (data) = self.clusters_id_add_host_post_with_http_info(id, **kwargs)
+            return data
+
+    def clusters_id_add_host_post_with_http_info(self, id, **kwargs):
+        """
+        Add a host to the specified cluster.
+        Add a host to the specified cluster. 
+
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please define a `callback` function
+        to be invoked when receiving the response.
+        >>> def callback_function(response):
+        >>>     pprint(response)
+        >>>
+        >>> thread = api.clusters_id_add_host_post_with_http_info(id, callback=callback_function)
+
+        :param callback function: The callback function
+            for asynchronous request. (optional)
+        :param int id: Id of the cluster (required)
+        :return: Cluster
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        all_params = ['id']
+        all_params.append('callback')
+        all_params.append('_return_http_data_only')
+
+        params = locals()
+        for key, val in iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method clusters_id_add_host_post" % key
+                )
+            params[key] = val
+        del params['kwargs']
+        # verify the required parameter 'id' is set
+        if ('id' not in params) or (params['id'] is None):
+            raise ValueError("Missing the required parameter `id` when calling `clusters_id_add_host_post`")
+
+        if 'id' in params and params['id'] < 0.0:
+            raise ValueError("Invalid value for parameter `id` when calling `clusters_id_add_host_post`, must be a value greater than or equal to `0.0`")
+        resource_path = '/clusters/{id}/add_host/'.replace('{format}', 'json')
+        path_params = {}
+        if 'id' in params:
+            path_params['id'] = params['id']
+
+        query_params = {}
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+
+        body_params = None
+
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.\
+            select_header_accept(['application/json'])
+        if not header_params['Accept']:
+            del header_params['Accept']
+
+        # HTTP header `Content-Type`
+        header_params['Content-Type'] = self.api_client.\
+            select_header_content_type(['application/json'])
+
+        # Authentication setting
+        auth_settings = []
+
+        return self.api_client.call_api(resource_path, 'POST',
+                                            path_params,
+                                            query_params,
+                                            header_params,
+                                            body=body_params,
+                                            post_params=form_params,
+                                            files=local_var_files,
+                                            response_type='Cluster',
+                                            auth_settings=auth_settings,
+                                            callback=params.get('callback'),
+                                            _return_http_data_only=params.get('_return_http_data_only'))
+
     def clusters_id_delete(self, id, **kwargs):
         """
         Delete an already existing cluster definition.
@@ -568,6 +674,112 @@ class ClusterDefinitionsApi(object):
         auth_settings = ['api_key', 'basic']
 
         return self.api_client.call_api(resource_path, 'PUT',
+                                            path_params,
+                                            query_params,
+                                            header_params,
+                                            body=body_params,
+                                            post_params=form_params,
+                                            files=local_var_files,
+                                            response_type='Cluster',
+                                            auth_settings=auth_settings,
+                                            callback=params.get('callback'),
+                                            _return_http_data_only=params.get('_return_http_data_only'))
+
+    def clusters_id_remove_host_post(self, id, **kwargs):
+        """
+        Remove a host from the specified cluster.
+        Remove a host from the specified cluster. 
+
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please define a `callback` function
+        to be invoked when receiving the response.
+        >>> def callback_function(response):
+        >>>     pprint(response)
+        >>>
+        >>> thread = api.clusters_id_remove_host_post(id, callback=callback_function)
+
+        :param callback function: The callback function
+            for asynchronous request. (optional)
+        :param int id: Id of the cluster (required)
+        :return: Cluster
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('callback'):
+            return self.clusters_id_remove_host_post_with_http_info(id, **kwargs)
+        else:
+            (data) = self.clusters_id_remove_host_post_with_http_info(id, **kwargs)
+            return data
+
+    def clusters_id_remove_host_post_with_http_info(self, id, **kwargs):
+        """
+        Remove a host from the specified cluster.
+        Remove a host from the specified cluster. 
+
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please define a `callback` function
+        to be invoked when receiving the response.
+        >>> def callback_function(response):
+        >>>     pprint(response)
+        >>>
+        >>> thread = api.clusters_id_remove_host_post_with_http_info(id, callback=callback_function)
+
+        :param callback function: The callback function
+            for asynchronous request. (optional)
+        :param int id: Id of the cluster (required)
+        :return: Cluster
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        all_params = ['id']
+        all_params.append('callback')
+        all_params.append('_return_http_data_only')
+
+        params = locals()
+        for key, val in iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method clusters_id_remove_host_post" % key
+                )
+            params[key] = val
+        del params['kwargs']
+        # verify the required parameter 'id' is set
+        if ('id' not in params) or (params['id'] is None):
+            raise ValueError("Missing the required parameter `id` when calling `clusters_id_remove_host_post`")
+
+        if 'id' in params and params['id'] < 0.0:
+            raise ValueError("Invalid value for parameter `id` when calling `clusters_id_remove_host_post`, must be a value greater than or equal to `0.0`")
+        resource_path = '/clusters/{id}/remove_host/'.replace('{format}', 'json')
+        path_params = {}
+        if 'id' in params:
+            path_params['id'] = params['id']
+
+        query_params = {}
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+
+        body_params = None
+
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.\
+            select_header_accept(['application/json'])
+        if not header_params['Accept']:
+            del header_params['Accept']
+
+        # HTTP header `Content-Type`
+        header_params['Content-Type'] = self.api_client.\
+            select_header_content_type(['application/json'])
+
+        # Authentication setting
+        auth_settings = []
+
+        return self.api_client.call_api(resource_path, 'POST',
                                             path_params,
                                             query_params,
                                             header_params,
