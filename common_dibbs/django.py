3fadba2442041eb4ad9dc9773b234a2266e66311
Nick Timkovich <prometheus235@gmail.com>
2017-01-24 12:16:39 -0600

    Add auth class for DRF (based on SessionAuth)

--- common_dibbs/django.py
+++ common_dibbs/django.py
@@ -13,8 +13,8 @@ from django.conf import settings
 from django.contrib.auth import backends as django_backends
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponseBadRequest
-
 import requests
+from rest_framework.authentication import BaseAuthentication
 
 from .misc import header_to_metakey
 from . import names
@@ -92,6 +92,29 @@ class InterserviceMiddleware(object):
             request.dibbs_interservice = True
         else:
             request.dibbs_interservice = False
+
+
+class DRFAuthentication(BaseAuthentication):
+    """
+    Mostly copied from rest_framework.authentication.SessionAuthentication
+    because we're using Django middleware machinery to do the authentication
+    anyways which sets ``request.user`` (for Django's request, not DRF's
+    wrapped request). This just ignores the CSRF stuff (shouldn't be
+    applicable if using tokens?)
+    """
+    def authenticate(self, request):
+        """
+        Returns a `User` if the request session currently has a logged in user.
+        Otherwise returns `None`.
+        """
+        # Get the session-based user from the underlying HttpRequest object
+        user = getattr(request._request, 'user', None)
+
+        # Unauthenticated
+        if not user or not user.is_active:
+            return None
+
+        return (user, None)
 
 
 def relay_headers(incoming_request):
